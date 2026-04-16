c7d3db0428caeb1064fb81888e80004ab4de232a
sammymourasato-hub <sammymourasato@gmail.com>
2026-04-16 20:02:31 -0300

    Initial commit

--- CP02-PENSAMENTO-COMPUTACIONAL-PYTHON/pcp_1sem_cp2_ex01.py
+++ CP02-PENSAMENTO-COMPUTACIONAL-PYTHON/pcp_1sem_cp2_ex01.py
@@ -11,21 +11,46 @@ print(f"Peso convertido em kilos: {peso_kg:.2f} Kg")
 # PREÇO DA CARGA POR KG -->
 def valor_carga_por_kg():
     if 10 <= cod_carga <= 20:
-        preco_carga1 = (peso_kg * 100.00)
-        print(f"O preço da carga é de: R${preco_carga1:.2f}")
+        return peso_kg * 100
 
     elif 21 <= cod_carga <= 30:
-        preco_carga2 = (peso_kg * 250)
-        print(f"O preço da carga é de: R${preco_carga2:.2f}")
+        return peso_kg * 250
 
     elif 31 <= cod_carga <= 40:
-        preco_carga3 = (peso_kg * 340)
-        print(f"O preço da carga é de: R${preco_carga3:.2f}")
+        return peso_kg * 340
 
     else:
-        ("ERRO!! Dígite números do código de 10 a 40")
+        return print("ERRO!! Dígite números do código de 10 a 40")
+
+print(f"O preço da carga: R${valor_carga_por_kg(): .2f}")
+
+# VALOR DOS IMPOSTOS
+def valor_imposto():
+    if cod_origem == 1:
+        return valor_carga_por_kg() * 0.35
+
+    elif cod_origem == 2:
+        return valor_carga_por_kg() * 0.25
+
+    elif cod_origem == 3:
+        return valor_carga_por_kg() * 0.15
+
+    elif cod_origem == 4:
+        return valor_carga_por_kg() * 0.05
+
+    elif cod_origem == 5:
+        return 0
+
+    else:
+        return print("Erro!! OS números do código de origem de 1 a 5")
+
+print(f"Valor do imposto: R${valor_imposto(): .2f}")
 
 # VALOR TOTAL TRANSPORTADO PELO CAMINHÃO
+def valor_total():
+    return valor_carga_por_kg() + valor_imposto()
+
+print(f"O valor total: R${valor_total(): .2f}")
 
 
 
